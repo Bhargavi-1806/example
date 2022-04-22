6e72c4e84f4aefd004c47b3456bbcb225ff10a51
Bhargavi-1806 <78410136+Bhargavi-1806@users.noreply.github.com>
2022-04-22 22:05:45 +0530

    Add files via upload

---
+++ as.js
@@ -0,0 +1,9 @@
+var change = document.getElementsByClassName("container")[0];
+function popup(){
+    change.style.visibility ="visible";
+    document.getElementById("login").style.visibility='hidden';
+}
+function c(){
+    change.style.visibility = "hidden";
+  
+}
